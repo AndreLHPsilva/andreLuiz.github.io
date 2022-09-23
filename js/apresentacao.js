31d3c571e9bc90250e5b15bffeef848b66cc0906
André Luiz <andrepadilha_@outlook.com>
2022-09-23 11:38:35 -0300

    mais adaptações

--- js/apresentacao.js
+++ js/apresentacao.js
@@ -5,7 +5,7 @@ export default function efeitoMaquinaEscrever(){
     textoArray.forEach((letra,i) => {
       setTimeout(function(){
         txt.innerHTML += letra;
-      }, 105 * i)
+      }, 85 * i)
       console.log(letra,i);
     })
   }
