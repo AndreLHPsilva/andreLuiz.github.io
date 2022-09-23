5afc6f1cbcbe7837df28f3f79bd210970bea9f31
André Luiz <andrepadilha_@outlook.com>
2022-09-23 11:28:39 -0300

    Modificando efeito maquina de escrever

--- js/apresentacao.js
+++ js/apresentacao.js
@@ -1,20 +1,14 @@
 export default function efeitoMaquinaEscrever(){
-
-
   function typeWrite(txt){
-    
-    const textoArray = txt.innerHTML.split('');
-    txt.innerHTML = '';
-    
+    const textoArray = txt.innerHTML.match(/((<|<\/)([\w]+)>)|(\D)/gi);
+    txt.innerText = '';
     textoArray.forEach((letra,i) => {
       setTimeout(function(){
         txt.innerHTML += letra;
       }, 75 * i)
+      console.log(letra,i);
     })
   }
   const h1 = document.querySelector('.apresentacao-texto');
   typeWrite(h1);
-
-  console.log(h1);
-
 }
