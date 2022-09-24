4287a1ac96a97b1446cc1d1b6035f9a94adbb42a
André Luiz <andrepadilha_@outlook.com>
2022-09-24 17:30:24 -0300

    mudando tempo do efeito

--- js/apresentacao.js
+++ js/apresentacao.js
@@ -2,7 +2,7 @@ export default function efeitoMaquinaEscrever(){
   const typed = new Typed('.apresentacao-texto', {
     // Waits 1000ms after typing "First"
     strings: ['Olá, bem vindo ao meu portfólio.<br>Me chamo <strong>André Luiz</strong> e almejo o<br>cargo de <strong>Estágio Desenvolvedor<br>Web.</strong>'],
-    typeSpeed: 105,
+    typeSpeed: 75,
     showCursor: false
   });
 }
