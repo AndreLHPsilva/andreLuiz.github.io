95df8ec5623782fcc132935c0ef4e584f0d98655
André Luiz <andrepadilha_@outlook.com>
2022-09-24 17:32:35 -0300

    adicionando palavra para genero feminino

--- js/apresentacao.js
+++ js/apresentacao.js
@@ -1,7 +1,7 @@
 export default function efeitoMaquinaEscrever(){
   const typed = new Typed('.apresentacao-texto', {
     // Waits 1000ms after typing "First"
-    strings: ['Olá, bem vindo ao meu portfólio.<br>Me chamo <strong>André Luiz</strong> e almejo o<br>cargo de <strong>Estágio Desenvolvedor<br>Web.</strong>'],
+    strings: ['Olá, bem vindo(a) ao meu portfólio.<br>Me chamo <strong>André Luiz</strong> e almejo o<br>cargo de <strong>Estágio Desenvolvedor<br>Web.</strong>'],
     typeSpeed: 75,
     showCursor: false
   });
