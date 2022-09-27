ee3d7a9e2f470a5b150c7bacb79b758947852455
André Luiz <andrepadilha_@outlook.com>
2022-09-27 12:14:14 -0300

    continuação do site

--- js/header.js
+++ js/header.js
@@ -5,5 +5,13 @@ export default function header(){
     const navMenu = document.querySelector('.header-container');
     navMenu.classList.toggle('active');
   }
+  const linkMenu = document.querySelectorAll('a[href^="#"');
+  function fecharMenu(event){
+    const navMenu = document.querySelector('.header-container');
+    navMenu.classList.remove('active');
+  }
+  linkMenu.forEach(link => {
+    link.addEventListener('click', fecharMenu);
+  })
   btnMobile.addEventListener('click', menuMobile);
 }
