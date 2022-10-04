9c986db0561af480f7034c1e217c95947e481715
André Luiz <andrepadilha_@outlook.com>
2022-10-04 12:16:32 -0300

    inseri scroll lateral para mobile

--- js/projeto.js
+++ js/projeto.js
@@ -1,10 +1,10 @@
 export default function girar(){
   const card = document.querySelectorAll('.card__giratorio');
+  console.log(card);
   function vira(Touch){
     const pegarTouch = Touch.target.offsetParent;
     pegarTouch.classList.toggle('view');
   }
-
   if (window.matchMedia("(max-width:600px)").matches) {
     card.forEach((card) =>{
       card.classList.toggle('card-mobile');
