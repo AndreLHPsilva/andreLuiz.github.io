f4f93f85251bd9b7eb9e56e0dd8c8d6909803e26
André Luiz <andrepadilha_@outlook.com>
2022-10-04 22:02:28 -0300

    adaptações na parte de projetos

--- js/projeto.js
+++ js/projeto.js
@@ -1,14 +1,25 @@
 export default function girar(){
   const card = document.querySelectorAll('.card__giratorio');
-  console.log(card);
+  // Quando clica no link veja mais entra nessa função e vira o card.
   function vira(Touch){
-    const pegarTouch = Touch.target.offsetParent;
-    pegarTouch.classList.toggle('view');
+    const pegarTouch = Touch.path[2];
+    const cardTraseiro = document.querySelectorAll('.card__giratorio-conteudo--traseira');
+    cardTraseiro.forEach((cardTraseiro) =>{
+      //aqui é para quando clicar dentro do conteudo da traseira, desvirar o card
+      cardTraseiro.addEventListener('touchstart', function(Touch){
+        pegarTouch.classList.remove('view');
+      });
+    })
+    pegarTouch.classList.add('view');
+
   }
   if (window.matchMedia("(max-width:600px)").matches) {
     card.forEach((card) =>{
       card.classList.toggle('card-mobile');
-      card.addEventListener('touchstart', vira);
+      const linkCard = document.querySelectorAll('.card__giratorio p');
+      linkCard.forEach((linkCard)=>{
+        linkCard.addEventListener('touchstart', vira);
+      })
     })
   } 
 }
