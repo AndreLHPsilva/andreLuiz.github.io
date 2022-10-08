d8ae83c05f8ebe6de2e0fbc1a77b1889fbaa92e4
André Luiz <andrepadilha_@outlook.com>
2022-10-07 21:54:49 -0300

    tentando arrumar bug dos links

---
+++ js/animacao.js
@@ -0,0 +1,9 @@
+// export default function animacao(){
+
+// }
+// const linkedin = document.querySelector('.linkedIn');
+
+// function addClasseLinkedin(){
+//   linkedin.classList.add('fadeInLeft');
+// }
+// setTimeout(addClasseLinkedin, 100)
