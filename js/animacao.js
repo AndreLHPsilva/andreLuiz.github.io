240e47937c26f1c9ae2ad39979f5289df7a8906f
André Luiz <andrepadilha_@outlook.com>
2022-10-08 09:47:15 -0300

    modificando animação

--- js/animacao.js
+++
@@ -1,9 +0,0 @@
-// export default function animacao(){
-
-// }
-// const linkedin = document.querySelector('.linkedIn');
-
-// function addClasseLinkedin(){
-//   linkedin.classList.add('fadeInLeft');
-// }
-// setTimeout(addClasseLinkedin, 100)
--- script.js
+++ script.js
@@ -2,11 +2,8 @@ import header from "./js/header.js";
 import efeitoMaquinaEscrever from "./js/apresentacao.js";
 import girar from "./js/projeto.js";
 import subir from "./js/subir.js";
-import animacao from "./js/animacao.js";
 
 header();
 efeitoMaquinaEscrever();
 girar();
 subir();
-new SimpleAnime();
-animacao();
